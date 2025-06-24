725994b4ec0214a9d5037c0d777eeacc2f773d16
Jackson Steely <161091774+jsteel17@users.noreply.github.com>
2025-06-24 23:09:56 +0000

    frontend

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/pt75-group1-final/',
+    base: './',
     server: {
         port: 3000
     },
